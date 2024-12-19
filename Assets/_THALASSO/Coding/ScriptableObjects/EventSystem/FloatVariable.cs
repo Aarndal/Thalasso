f4f6f7afe4526b932ff0f10490671f0f77f892cb
Aarndal <aarndal@duck.com>
2024-12-19 16:03:17 +0100

    Added ScriptableObjects for Scriptable Object Events. Created Test scene for EventSystem.

---
+++ Assets/_THALASSO/Coding/ScriptableObjects/EventSystem/FloatVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scribtable Float Variable", fileName = "newScribtableFloatVariable")]
+public class FloatVariable : ScriptableObject
+{
+    public float value;
+}
