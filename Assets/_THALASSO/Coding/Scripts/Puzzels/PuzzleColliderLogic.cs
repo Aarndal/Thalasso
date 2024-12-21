0ee521c957725ff6dc4d9efda31973c413de926f
Pascal Keller <Pascal.keller12@gmail.com>
2024-12-21 22:15:47 +0100

    Added input blocking for UI interactions

--- Assets/_THALASSO/Coding/Scripts/Puzzels/PuzzleColliderLogic.cs
+++ Assets/_THALASSO/Coding/Scripts/Puzzels/PuzzleColliderLogic.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,15 +10,19 @@ public class PuzzleColliderLogic : MonoBehaviour
     [SerializeField] private float transitionduration;
     [SerializeField] private AnimationCurve animationSpeedCurve;
 
+
     private PlayerInput playerInput;
 
     private Coroutine curCoroutine;
     private Vector3 originTransformPosition;
     private Quaternion originTransformRotation;
+    private GameObject transform;
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
 
     private void Start()
     {
         playerInput = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>();
+        cinemachineVirtualCamera = GameObject.FindAnyObjectByType<CinemachineVirtualCamera>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -48,14 +53,22 @@ public class PuzzleColliderLogic : MonoBehaviour
                     originTransformPosition = Camera.main.transform.position;
                     originTransformRotation = Camera.main.transform.rotation;
 
-                    CamTransitionSystem.Instance.TransitionPosRot(Camera.main.gameObject, targetCamera, transitionduration, animationSpeedCurve, () => playerInput.ActivateInput(), () => isfocused = true);
+                    CamTransitionSystem.Instance.TransitionPosRot(Camera.main.gameObject, targetCamera, transitionduration, animationSpeedCurve, () =>
+                    {
+                        cinemachineVirtualCamera.enabled = false;
+                        playerInput.DeactivateInput();
+                    }, () =>
+                    {
+                        isfocused = true;
+                    });
                 }
                 else
                 {
-                    CamTransitionSystem.Instance.TransitionPosRot(Camera.main.gameObject, PosRotToTransform(originTransformPosition,originTransformRotation), transitionduration, animationSpeedCurve, null, () =>
+                    CamTransitionSystem.Instance.TransitionPosRot(Camera.main.gameObject, PosRotToTransform(originTransformPosition, originTransformRotation), transitionduration, animationSpeedCurve, null, () =>
                     {
+                        cinemachineVirtualCamera.enabled = true;
                         isfocused = false;
-                        playerInput.DeactivateInput();
+                        playerInput.ActivateInput();
                     });
                 }
             }
@@ -65,9 +78,19 @@ public class PuzzleColliderLogic : MonoBehaviour
 
     public Transform PosRotToTransform(Vector3 position, Quaternion rotation)
     {
-        GameObject transform = new GameObject("temp");
+        if (transform == null)
+            transform = new GameObject("temp");
+
         transform.transform.position = position;
         transform.transform.rotation = rotation;
         return transform.transform;
+    }
+
+    private void OnDestroy()
+    {
+        if (transform != null)
+        {
+            Destroy(transform);
+        }
     }
 }
