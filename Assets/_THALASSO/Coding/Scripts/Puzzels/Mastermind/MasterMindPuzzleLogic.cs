9c2c8335656ecfee43f2e162c03ebaa99fcb3bb5
Pascal Keller <Pascal.keller12@gmail.com>
2024-12-30 17:38:01 +0100

    Created new folder and scene as well as logic script

---
+++ Assets/_THALASSO/Coding/Scripts/Puzzels/Mastermind/MasterMindPuzzleLogic.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterMindPuzzleLogic : MonoBehaviour
+{
+    [SerializeField] private Button[] numButtons;
+    [SerializeField] private TextMeshProUGUI[] outputText;
+
+    private void Start()
+    {
+        
+    }
+
+    public void NumButtonInput(int _num)
+    {
+
+    }
+}
