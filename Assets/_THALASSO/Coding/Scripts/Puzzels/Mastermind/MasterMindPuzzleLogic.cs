4e0836a3ad9b690f501c142edcd429341aae2672
Pascal Keller <Pascal.keller12@gmail.com>
2025-01-03 18:52:52 +0100

    script update do support 0 and manual check and reset

--- Assets/_THALASSO/Coding/Scripts/Puzzels/Mastermind/MasterMindPuzzleLogic.cs
+++ Assets/_THALASSO/Coding/Scripts/Puzzels/Mastermind/MasterMindPuzzleLogic.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,23 +12,25 @@ public class MasterMindPuzzleLogic : MonoBehaviour
     private int[] code = new int[4];
     private int[] inputCode = new int[4];
 
-    private bool inputLocked = false;
 
 
     private void Start()
     {
         GenerateNewCode();
-        for (int i = 0; i < numButtons.Length; i++)
+        for (int i = 0; i < numButtons.Length - 3; i++)
         {
             int buttonIndex = i + 1;
             numButtons[i].onClick.AddListener(() => NumButtonInput(buttonIndex));
         }
+        numButtons[9].onClick.AddListener(() => CheckInput());
+        numButtons[10].onClick.AddListener(() => NumButtonInput(0));
+        numButtons[11].onClick.AddListener(() => Reset());
     }
 
     private void GenerateNewCode()
     {
         System.Random rnd = new System.Random();
-        List<int> availableNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List<int> availableNumbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         for (int i = 0; i < code.Length; i++)
         {
@@ -36,6 +38,7 @@ public class MasterMindPuzzleLogic : MonoBehaviour
             code[i] = availableNumbers[randomIndex];
             availableNumbers.RemoveAt(randomIndex);
         }
+        Debug.Log(code);
     }
 
     private void OnDestroy()
@@ -46,29 +49,18 @@ public class MasterMindPuzzleLogic : MonoBehaviour
     int inputIndex = 0;
     public void NumButtonInput(int _num)
     {
-        if(inputLocked) 
-            return;
 
-        if (inputIndex == 3)
-        {
-            inputCode[inputIndex] = _num;
-            outputText[inputIndex].text = _num.ToString();
-            CheckInput();
-            inputIndex = 0;
-        }
-        else
+        if (inputIndex <= 3)
         {
             inputCode[inputIndex] = _num;
             outputText[inputIndex].text = _num.ToString();
             inputIndex++;
         }
-
     }
 
     private void CheckInput()
     {
         bool allCorrect = true;
-        inputLocked = true;
 
         for (int i = 0; i < inputCode.Length; i++)
         {
@@ -76,7 +68,7 @@ public class MasterMindPuzzleLogic : MonoBehaviour
             {
                 outputText[i].color = Color.green;
             }
-            else if (System.Array.Exists(code, element => element == inputCode[i]))
+            else if (code.Contains(inputCode[i]))
             {
                 outputText[i].color = Color.yellow;
                 allCorrect = false;
@@ -92,10 +84,6 @@ public class MasterMindPuzzleLogic : MonoBehaviour
         {
             PuzzleSolved();
         }
-        else
-        {
-            StartCoroutine(ResetAfterSeconds(3));
-        }
     }
 
     private void PuzzleSolved()
@@ -103,9 +91,8 @@ public class MasterMindPuzzleLogic : MonoBehaviour
         Debug.Log("Puzzle solved!");
     }
 
-    private IEnumerator ResetAfterSeconds(int seconds)
+    private void Reset()
     {
-        yield return new WaitForSeconds(seconds);
 
         inputIndex = 0;
         for (int i = 0; i < outputText.Length; i++)
@@ -114,6 +101,5 @@ public class MasterMindPuzzleLogic : MonoBehaviour
             outputText[i].color = Color.white;
         }
 
-        inputLocked = false;
     }
 }
