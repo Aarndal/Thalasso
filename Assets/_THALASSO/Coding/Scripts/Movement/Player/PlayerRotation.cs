3184eb4c0dc822f06e426c80d830565aceaec2cf
Aaron Dahlhauser <aarndal@duck.com>
2025-01-08 23:04:42 +0100

    PlayerRotation fixes. Made rotation in both direction smoother. Fixed top and bottomClampAngle being inverted.

--- Assets/_THALASSO/Coding/Scripts/Movement/Player/PlayerRotation.cs
+++ Assets/_THALASSO/Coding/Scripts/Movement/Player/PlayerRotation.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
+    
     [Header("References")]
     [SerializeField]
     private PlayerInputReader _input = default;
@@ -11,7 +12,8 @@ public class PlayerRotation : MonoBehaviour
 
     [Header("Look Variables")]
     [SerializeField]
-    private const float ROTATION_TRESHOLD = 0.2f;
+    [Range(1.0f, 2.0f)]
+    private float _rotationThreshold = 1.0f;
     [Tooltip("How far in degrees can you move the camera up.")]
     [SerializeField]
     private float _topClampAngle = 80.0f;
@@ -23,21 +25,23 @@ public class PlayerRotation : MonoBehaviour
     [Tooltip("How smooth the player character rotates towards the look direction.")]
     [SerializeField]
     [Range(0.1f, 0.9f)]
-    private float _rotationSmoothFactor = 0.35f;
+    private float _yawSmoothFactor =  0.5f;
+    [SerializeField]
+    [Range(0.1f, 0.9f)]
+    private float _pitchSmoothFactor = 0.5f;
     [Tooltip("How fast the player character can rotate left and right.")]
     [SerializeField]
     [Range(0.1f, 10.0f)]
     private float _horizontalSensitivity = 1.0f;
     [Tooltip("How fast the player character can look up and down.")]
     [SerializeField]
-    [Range(0.1f, 10.0f)]
-    private float _verticalSensitivity = 1.0f;
+    [Range(0.01f, 1.0f)]
+    private float _verticalSensitivity = 0.1f;
 
     private Vector3 _lookDirection = Vector3.zero;
     private Vector3 _previousLookDirection = Vector3.zero;
     private float _pitch = 0.0f;
     private float _yaw = 0.0f;
-    private float _rotationVelocity;
 
     public Transform CameraRoot { get => _cameraRoot; }
 
@@ -74,40 +78,31 @@ public class PlayerRotation : MonoBehaviour
 
     private void OnLookInputHasChanged(Vector2 lookInput)
     {
-
         _previousLookDirection = _lookDirection;
-
-        //if (lookInput.sqrMagnitude <= ROTATION_TRESHOLD)
-        //{
-        //    _lookDirection = Vector3.zero;
-        //    return;
-        //}
 
         _lookDirection = new(
         x: (!_input.LookXInputIsInverted ? lookInput.x : -lookInput.x),
-        y: (!_input.LookYInputIsInverted ? lookInput.y : -lookInput.y),
+        y: (_input.LookYInputIsInverted ? lookInput.y : -lookInput.y),
         z: 0.0f);
     }
 
     private void CameraRotation()
     {
-        if (_lookDirection.sqrMagnitude >= ROTATION_TRESHOLD)
+        if (_lookDirection.sqrMagnitude >= _rotationThreshold)
         {
             ////Don't multiply mouse input by Time.deltaTime
             //float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
 
             float deltaTimeMultiplier = 1.0f;
 
-            _pitch += _lookDirection.y * _verticalSensitivity * deltaTimeMultiplier;
-            _yaw += _lookDirection.x * _horizontalSensitivity * deltaTimeMultiplier;
+            _pitch += Mathf.LerpAngle(_previousLookDirection.y, _lookDirection.y, _pitchSmoothFactor) * _verticalSensitivity * deltaTimeMultiplier;
+            _yaw += Mathf.LerpAngle(_previousLookDirection.x, _lookDirection.x, _yawSmoothFactor) * _horizontalSensitivity * deltaTimeMultiplier;
 
-            // clamp our pitch rotation
-            _pitch = ClampAngle(_pitch, _bottomClampAngle, _topClampAngle);
+            //_pitch = ClampAngle(_pitch, _bottomClampAngle, _topClampAngle);
+            _pitch = Mathf.Clamp(_pitch, _bottomClampAngle, _topClampAngle);
 
-            // Update camera target pitch
-            _cameraRoot.localRotation = Quaternion.Euler(_pitch, 0.0f, 0.0f);
+            _cameraRoot.localRotation = Quaternion.Euler(-_pitch, 0.0f, 0.0f);
 
-            // rotate the player left and right
             this.transform.localRotation = Quaternion.Euler(0.0f, _yaw, 0.0f);
         }
     }
