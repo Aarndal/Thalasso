ffe4e5ce8067573109bd716a62b7f0212c3d0ba8
Aaron Dahlhauser <aarndal@duck.com>
2025-01-23 22:01:07 +0100

    Deleted Assemblies for UnitTesting and CreateFolder script. Added Test build. Init SO_ProgressionTracker script. Build successful for Test_FPSController scene.

--- Assets/_THALASSO/Coding/Scripts/Movement/PlayerCharacter/PCRotation.cs
+++ Assets/_THALASSO/Coding/Scripts/Movement/PlayerCharacter/PCRotation.cs
@@ -13,7 +13,7 @@ public class PCRotation : MonoBehaviour
 
     [Header("Look Variables")]
     [Tooltip("Defines the minimum threshold value for registering the look input for the rotation. Is compared with the squared magnitude of the look input vector.")]
-    [SerializeField, Range(0.1f, 10.0f)]
+    [SerializeField, Range(0.01f, 1.0f)]
     private float _rotationThreshold = 1.0f;
     [Tooltip("How far in degrees can you move the camera up.")]
     [SerializeField, Range(30.0f, 80.0f)]
---
+++ Assets/_THALASSO/Coding/Scripts/ProgressionTracking/Base/SO_ProgressionTracker.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProgressionTracking
+{
+    [CreateAssetMenu(fileName = "NewProgressionTracker", menuName = "Scriptable Objects/Progression Tracker")]
+    public class SO_ProgressionTracker : ScriptableObject
+    {
+        [SerializeField]
+        private uint _id = 0;
+
+        [SerializeField]
+        private List<uint> _solvableDependenciesID = new();
+
+        private bool _isCompleted = false;
+        private Dictionary<uint, bool> _progression = new();
+
+        public uint ID => _id;
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            private set
+            {
+                if (_isCompleted != value && value == true)
+                {
+                    _isCompleted = value;
+                    GlobalEventBus.Raise(GlobalEvents.Game.ProgressionCompleted, _id);
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            if (_solvableDependenciesID.Count > 0)
+                foreach (var id in _solvableDependenciesID)
+                    _progression.Add(id, false);
+        }
+
+        private void OnEnable()
+        {
+            foreach (var id in _progression)
+                GlobalEventBus.Register(GlobalEvents.Game.HasBeenSolved, OnHasBeenSolved);
+        }
+
+        private void OnDisable()
+        {
+            foreach (var id in _progression)
+                GlobalEventBus.Deregister(GlobalEvents.Game.HasBeenSolved, OnHasBeenSolved);
+        }
+
+        private void OnHasBeenSolved(object[] args)
+        {
+            if (args[0] is uint id)
+            {
+                _progression[id] = true;
+
+                CheckProgression();
+            }
+        }
+
+        private void CheckProgression()
+        {
+            IsCompleted = System.Linq.Enumerable.All(_progression, (o) => o.Value);
+        }
+    }
+}
