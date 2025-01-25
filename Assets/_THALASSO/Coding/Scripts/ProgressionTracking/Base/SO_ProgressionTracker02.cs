769cde1e200f555e4c70c0b1c5c0fd166513f32e
Aaron Dahlhauser <aarndal@duck.com>
2025-01-25 22:15:50 +0100

    New ProgressionTracker for testing.

---
+++ Assets/_THALASSO/Coding/Scripts/ProgressionTracking/Base/SO_ProgressionTracker02.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProgressionTracking
+{
+    [CreateAssetMenu(fileName = "NewProgressionTracker", menuName = "Scriptable Objects/Progression Tracker")]
+    public class SO_ProgressionTracker02 : SO_Singleton
+    {
+        [SerializeField]
+        private uint _id = 0;
+
+        [SerializeField]
+        private List<SO_SolvableObject> _solvableDependenciesID = new();
+
+        [SerializeField]
+        private bool _isCompleted = false;
+
+        private readonly Dictionary<uint, bool> _progression = new();
+
+        public uint ID => _id;
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            private set
+            {
+                if (_isCompleted != value)
+                {
+                    _isCompleted = value;
+
+                    //if (_isCompleted)
+                    //    GlobalEventBus.Raise(GlobalEvents.Game.ProgressionCompleted, _id);
+                }
+            }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (_solvableDependenciesID.Count > 0 && _solvableDependenciesID.Count != _progression.Count)
+                foreach (var solvableObject in _solvableDependenciesID)
+                    _progression.Add(solvableObject.ID, solvableObject.IsSolved);
+        }
+
+        private void OnEnable()
+        {
+            GlobalEventBus.Register(GlobalEvents.Game.HasBeenSolved, OnHasBeenSolved);
+        }
+
+        private void OnDisable()
+        {
+            GlobalEventBus.Deregister(GlobalEvents.Game.HasBeenSolved, OnHasBeenSolved);
+
+#if UNITY_EDITOR
+            IsCompleted = false;
+#endif
+        }
+
+        private void OnValidate()
+        {
+            if (_solvableDependenciesID.Count > 0 && _solvableDependenciesID.Count != _progression.Count)
+            {
+                _progression.Clear();
+                foreach (var solvableObject in _solvableDependenciesID)
+                    _progression.Add(solvableObject.ID, solvableObject.IsSolved);
+            }
+        }
+
+        private void OnHasBeenSolved(object[] args)
+        {
+            if (args[0] is uint id)
+            {
+                _progression[id] = true;
+                CheckProgression();
+            }
+        }
+
+        private void CheckProgression()
+        {
+            if (IsCompleted = System.Linq.Enumerable.All(_progression, (o) => o.Value))
+            {
+                GlobalEventBus.Raise(GlobalEvents.Game.ProgressionCompleted, _id);
+                Debug.Log("Progression of " + _id + " has been completed.");
+            }
+        }
+    }
+}
