1dce4fbe2464e4af5f414a65822dfb82b76844c0
Pascal Keller <Pascal.keller12@gmail.com>
2025-02-20 21:54:00 +0100

    Removed old junk hihi :)

--- Assets/_THALASSO/Coding/Scripts/Puzzels/ElectricityPuzzel/ElectricityPuzzleLogic.cs
+++ Assets/_THALASSO/Coding/Scripts/Puzzels/ElectricityPuzzel/ElectricityPuzzleLogic.cs
@@ -22,15 +22,12 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
     private GameObject startTile;
     private GameObject endTile;
     private GameObject buttonUICanvas;
-    private int failBuffer = 0;
     private System.Random rnd = new();
     private ElectricityPuzzlePrefab ativeLayoutPrefab;
 
     private GameObject[,] tileField = new GameObject[5, 3];
     private Button[] tileFieldButtonsInput;
     private List<Vector2Int> activeTiles = new();
-    private List<Vector2Int> updatedInThisLoop = new();
-    private List<Vector2Int> connectiongTiles = new();
 
     #region Unity Lifecycle Methods
     private void Awake()
@@ -49,7 +46,6 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
 
         ProcessTileFieldUpdate(tileField[0, 0]);
     }
-
 
     private void OnDestroy()
     {
@@ -127,151 +123,14 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
             }
         }
     }
+
     private void SetNewTileType(GameObject _tileObject, int _tileTypeIndice)
     {
-
         GameObject newTileType = differentTileTypes[_tileTypeIndice];
 
         _tileObject.GetComponent<MeshFilter>().mesh = newTileType.GetComponent<MeshFilter>().sharedMesh;
         _tileObject.name = newTileType.name;
     }
-
-    #region OldLogicWithBugs
-    private void GenerateNewLayoutOld()
-    {
-        connectiongTiles.Clear();
-        connectiongTiles.Add(ObjToPos(startTile));
-        failBuffer = 0;
-
-        ChooseNextTileOld(startTile);
-        BuildPathWithTilesOld();
-        CompleteRestOfTilesOld();
-    }
-
-    private void ChooseNextTileOld(GameObject curTile)
-    {
-        if (connectiongTiles.Count == 0 || failBuffer > 50)
-        {
-            GenerateNewLayoutOld();
-            return;
-        }
-
-        List<GameObject> neighboursTiles = GetAllNeighbours(curTile).FindAll(tile => !connectiongTiles.Contains(ObjToPos(tile)));
-
-        if (neighboursTiles.Count == 0)
-        {
-            if (curTile == startTile)
-            {
-                GenerateNewLayoutOld();
-                return;
-            }
-            failBuffer++;
-            connectiongTiles.RemoveAt(connectiongTiles.Count - 1);
-            ChooseNextTileOld(PosToObj(connectiongTiles[connectiongTiles.Count - 1]));
-            return;
-        }
-
-        GameObject newTile = neighboursTiles[rnd.Next(neighboursTiles.Count)];
-        connectiongTiles.Add(ObjToPos(newTile));
-
-        if (newTile == endTile)
-        {
-            return;
-        }
-
-        ChooseNextTileOld(newTile);
-    }
-
-    private void BuildPathWithTilesOld()
-    {
-        for (int i = 0; i < connectiongTiles.Count; i++)
-        {
-            List<Directions> neededDirection = new();
-            if (PosToObj(connectiongTiles[i]) == startTile)
-            {
-                neededDirection.Add(Directions.U);
-                neededDirection.Add(GetConnectionDirection(PosToObj(connectiongTiles[i]), PosToObj(connectiongTiles[i + 1])));
-            }
-            else if (PosToObj(connectiongTiles[i]) == endTile)
-            {
-                neededDirection.Add(Directions.D);
-                neededDirection.Add(GetConnectionDirection(PosToObj(connectiongTiles[i]), PosToObj(connectiongTiles[i - 1])));
-            }
-            else
-            {
-                neededDirection.Add(GetConnectionDirection(PosToObj(connectiongTiles[i]), PosToObj(connectiongTiles[i + 1])));
-                neededDirection.Add(GetConnectionDirection(PosToObj(connectiongTiles[i]), PosToObj(connectiongTiles[i - 1])));
-            }
-
-            ChooseTileBasedOnRequirementsOld(PosToObj(connectiongTiles[i]), neededDirection.ToArray());
-        }
-    }
-
-    private void ChooseTileBasedOnRequirementsOld(GameObject _tileObject, Directions[] _requiredDirections)
-    {
-        int directionOneValue = (int)_requiredDirections[0];
-        int directionTwoValue = (int)_requiredDirections[1];
-        int tileTypeValue;
-        if (Math.Abs(directionOneValue - directionTwoValue) > 3)
-        {
-            tileTypeValue = Math.Abs(3 - (Math.Abs(directionOneValue - directionTwoValue)));
-            Debug.LogWarning("over" + tileTypeValue);
-        }
-        else
-        {
-            tileTypeValue = Math.Abs(directionOneValue - directionTwoValue);
-            Debug.LogWarning("under" + tileTypeValue);
-        }
-        switch (tileTypeValue)
-        {
-            case 1:
-                {
-                    SetNewTileTypeOld(_tileObject, new int[] { 2, 3 });
-                    break;
-                }
-            case 2:
-                {
-                    SetNewTileTypeOld(_tileObject, new int[] { 1, 4 });
-                    break;
-                }
-            case 3:
-                {
-                    SetNewTileTypeOld(_tileObject, new int[] { 0 });
-                    break;
-                }
-        }
-    }
-
-    private void SetNewTileTypeOld(GameObject _tileObject, int[] _tileTypeIndices)
-    {
-        int randomSelection = rnd.Next(_tileTypeIndices.Length);
-
-        GameObject newTileType = differentTileTypes[_tileTypeIndices[randomSelection]];
-
-        _tileObject.GetComponent<MeshFilter>().mesh = newTileType.GetComponent<MeshFilter>().sharedMesh;
-        _tileObject.name = newTileType.name;
-    }
-
-    private void CompleteRestOfTilesOld()
-    {
-        for (int row = 0; row < tileField.GetLength(0); row++)
-        {
-            for (int col = 0; col < tileField.GetLength(1); col++)
-            {
-                Vector2Int pos = new Vector2Int(row, col);
-                if (!connectiongTiles.Contains(pos))
-                {
-                    GameObject tile = tileField[row, col];
-                    int randomIndex = rnd.Next(differentTileTypes.Length);
-                    GameObject newTileType = differentTileTypes[randomIndex];
-
-                    tile.GetComponent<MeshFilter>().mesh = newTileType.GetComponent<MeshFilter>().sharedMesh;
-                    tile.name = newTileType.name;
-                }
-            }
-        }
-    }
-    #endregion
 
     public void ProcessTileFieldUpdate(GameObject _updatedTile)
     {
@@ -285,7 +144,6 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
         Directions[] typeDirections = GetConnectionsForTileType(_updatedTile);
         Directions[] relativeDirections = ConvertDefaultConnectionIntoRotationRelative(typeDirections, rotSteps);
 
-
         if (_updatedTile == startTile)
         {
             if (relativeDirections.Contains(Directions.U))
@@ -293,7 +151,7 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
                 if (!activeTiles.Contains(ObjToPos(_updatedTile)))
                 {
                     activeTiles.Add(ObjToPos(_updatedTile));
-                    _updatedTile.GetComponent<MeshRenderer>().material.color = Color.green;                 //temp visuals
+                    _updatedTile.GetComponent<MeshRenderer>().material.color = Color.green; // temp visuals
                 }
 
                 Directions neededDirection = Directions.U;
@@ -329,9 +187,8 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
                     if (!activeTiles.Contains(ObjToPos(_updatedTile)))
                     {
                         activeTiles.Add(ObjToPos(_updatedTile));
-                        _updatedTile.GetComponent<MeshRenderer>().material.color = Color.green;                 //temp visuals
+                        _updatedTile.GetComponent<MeshRenderer>().material.color = Color.green; // temp visuals
                     }
-
 
                     nextTile = GetNextTileInCircuit(_updatedTile, relativeDirections, previousTile);
 
@@ -353,10 +210,7 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
             }
 
             DeactivateTile(_updatedTile);
-
         }
-
-
     }
 
     private void DeactivateTile(GameObject _tileToDeactivate)
@@ -456,233 +310,30 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
             return null;
         }
     }
-    #region oldCode
-    public void OnFieldGotUpdateOld(GameObject _updatedTile)
-    {
-        if (updatedInThisLoop.Contains(ObjToPos(_updatedTile)))
-        {
-            return;
-        }
-        else
-        {
-            updatedInThisLoop.Add(ObjToPos(_updatedTile));
-        }
 
-        int updatedTileCurRotation = Math.Abs(_updatedTile.GetComponent<PuzzleTileRotator>().curRotation);
-
-
-        if (_updatedTile == startTile)
-        {
-            int rotSteps = GetRotationSteps(updatedTileCurRotation);
-            Directions[] typeDirections = GetConnectionsForTileType(_updatedTile);
-            Directions[] relativeDirections = ConvertDefaultConnectionIntoRotationRelative(typeDirections, rotSteps);
-
-            if (relativeDirections.Contains(Directions.U))
-            {
-                _updatedTile.GetComponent<MeshRenderer>().material.color = Color.green;                 //temp visuals
-
-                if (!activeTiles.Contains(ObjToPos(_updatedTile)))
-                {
-                    activeTiles.Add(ObjToPos(_updatedTile));
-                }
-
-
-                updatedInThisLoop.Add(ObjToPos(_updatedTile));
-                UpdateNeighbour(_updatedTile);
-            }
-            else
-            {
-                _updatedTile.GetComponent<MeshRenderer>().material.color = Color.red;                 //temp visuals
-
-                if (activeTiles.Contains(ObjToPos(_updatedTile)))
-                {
-                    activeTiles.Remove(ObjToPos(_updatedTile));
-                    UpdateNeighbour(_updatedTile);
-                }
-            }
-        }
-        else if (_updatedTile == endTile)
-        {
-            int rotSteps = GetRotationSteps(updatedTileCurRotation);
-            Directions[] typeDirections = GetConnectionsForTileType(_updatedTile);
-            Directions[] relativeDirections = ConvertDefaultConnectionIntoRotationRelative(typeDirections, rotSteps);
-
-            List<GameObject> neighbours = GetAllNeighbours(_updatedTile);
-
-            bool hasActiveConnection = false;
-
-            foreach (GameObject neighbour in neighbours)
-            {
-                if (activeTiles.Contains(ObjToPos(neighbour)))
-                {
-
-                    if (OriginHasConnectionToTarget(neighbour, _updatedTile) && OriginHasConnectionToTarget(_updatedTile, neighbour) && relativeDirections.Contains(Directions.D))
-                    {
-                        if (activeTiles.Contains(ObjToPos(_updatedTile)))
-                            if (activeTiles.IndexOf(ObjToPos(neighbour)) > activeTiles.IndexOf(ObjToPos(_updatedTile)))
-                            {
-                                _updatedTile.GetComponent<MeshRenderer>().material.color = Color.red;    //temp visuals
-                                activeTiles.Remove(ObjToPos(_updatedTile));
-                                UpdateNeighbour(_updatedTile);
-                                return;
-                            }
-                        _updatedTile.GetComponent<MeshRenderer>().material.color = Color.green;               //temp visuals
-
-                        if (!activeTiles.Contains(ObjToPos(_updatedTile)))
-                        {
-                            activeTiles.Add(ObjToPos(_updatedTile));
-                        }
-
-
-                        updatedInThisLoop.Add(ObjToPos(_updatedTile));
-                        UpdateNeighbour(_updatedTile);
-
-                        hasActiveConnection = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!hasActiveConnection)
-            {
-                _updatedTile.GetComponent<MeshRenderer>().material.color = Color.red;    //temp visuals
-
-                if (activeTiles.Contains(ObjToPos(_updatedTile)))
-                {
-                    activeTiles.Remove(ObjToPos(_updatedTile));
-                    UpdateNeighbour(_updatedTile);
-                }
-            }
-
-        }
-        else
-        {
-            List<GameObject> neighbours = GetAllNeighbours(_updatedTile);
-
-            bool hasActiveConnection = false;
-
-            foreach (GameObject neighbour in neighbours)
-            {
-                if (activeTiles.Contains(ObjToPos(neighbour)))
-                {
-
-                    if (OriginHasConnectionToTarget(neighbour, _updatedTile) && OriginHasConnectionToTarget(_updatedTile, neighbour))
-                    {
-                        if (activeTiles.Contains(ObjToPos(_updatedTile)))
-                            if (activeTiles.IndexOf(ObjToPos(neighbour)) > activeTiles.IndexOf(ObjToPos(_updatedTile)))
-                            {
-                                _updatedTile.GetComponent<MeshRenderer>().material.color = Color.red;    //temp visuals
-                                activeTiles.Remove(ObjToPos(_updatedTile));
-                                UpdateNeighbour(_updatedTile);
-                                return;
-                            }
-                        _updatedTile.GetComponent<MeshRenderer>().material.color = Color.green;               //temp visuals
-
-                        if (!activeTiles.Contains(ObjToPos(_updatedTile)))
-                        {
-                            activeTiles.Add(ObjToPos(_updatedTile));
-                        }
-
-                        updatedInThisLoop.Add(ObjToPos(_updatedTile));
-                        UpdateNeighbour(_updatedTile);
-
-                        hasActiveConnection = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!hasActiveConnection)
-            {
-                _updatedTile.GetComponent<MeshRenderer>().material.color = Color.red;         //temp visuals
-
-                if (activeTiles.Contains(ObjToPos(_updatedTile)))
-                {
-                    activeTiles.Remove(ObjToPos(_updatedTile));
-                    UpdateNeighbour(_updatedTile);
-                }
-            }
-
-        }
-
-        if (updatedInThisLoop.Count != 0)
-            if (updatedInThisLoop[0] == ObjToPos(_updatedTile))
-            {
-                updatedInThisLoop.Clear();
-
-                if (activeTiles.Contains(ObjToPos(endTile)))
-                {
-                    Solve();
-                }
-            }
-    }
-    #endregion
-
-    private void UpdateNeighbour(GameObject _updatedTile)
-    {
-        List<GameObject> neighbours = GetAllNeighbours(_updatedTile);
-        foreach (GameObject neighbour in neighbours)
-            if (!updatedInThisLoop.Contains(ObjToPos(neighbour)))
-                ProcessTileFieldUpdate(neighbour);
-    }
-
-    #region Neighbours
-    private List<GameObject> GetAllNeighbours(GameObject _updatedTile)
-    {
-        List<GameObject> neighbours = new();
-
-        Vector2Int pos = ObjToPos(_updatedTile);
-
-        bool isOddY = pos.y % 2 == 0;
-
-        CheckAndAddNeighbour(pos.x - 1, pos.y, neighbours);                             // up
-
-        CheckAndAddNeighbour(isOddY ? pos.x : pos.x - 1, pos.y + 1, neighbours);        // right up
-        CheckAndAddNeighbour(isOddY ? pos.x + 1 : pos.x, pos.y + 1, neighbours);    // right down
-
-        CheckAndAddNeighbour(pos.x + 1, pos.y, neighbours);                             // down
-
-        CheckAndAddNeighbour(isOddY ? pos.x + 1 : pos.x, pos.y - 1, neighbours);          // left down
-        CheckAndAddNeighbour(isOddY ? pos.x : pos.x - 1, pos.y - 1, neighbours);         // left up
-
-        return neighbours;
-    }
-
-    private void CheckAndAddNeighbour(float _neighborRow, float _neighborCol, List<GameObject> _neighbours)
-    {
-        if (_neighborRow >= 0 && _neighborRow < tileField.GetLength(0) &&
-            _neighborCol >= 0 && _neighborCol < tileField.GetLength(1))
-        {
-            GameObject neighborTile = tileField[(int)_neighborRow, (int)_neighborCol];
-            if (neighborTile != null)
-            {
-                _neighbours.Add(neighborTile);
-            }
-        }
-    }
     private Directions GetConnectionDirection(GameObject _originTile, GameObject _targetTile)
     {
         Vector2Int posOrigin = ObjToPos(_originTile);
         Vector2Int posTarget = ObjToPos(_targetTile);
 
-
         bool isOddY = posOrigin.y % 2 == 0;
 
-        if (posOrigin.x - 1 == posTarget.x && posOrigin.y == posTarget.y)                               // Up
+        if (posOrigin.x - 1 == posTarget.x && posOrigin.y == posTarget.y) // Up
             return Directions.U;
         if (posOrigin.x == (isOddY ? posTarget.x : posTarget.x + 1) && posOrigin.y == posTarget.y - 1) // Right Up
             return Directions.RU;
         if (posOrigin.x == (isOddY ? posTarget.x - 1 : posTarget.x) && posOrigin.y == posTarget.y - 1) // Right Down
             return Directions.RD;
-        if (posOrigin.x + 1 == posTarget.x && posOrigin.y == posTarget.y)                              // Down
+        if (posOrigin.x + 1 == posTarget.x && posOrigin.y == posTarget.y) // Down
             return Directions.D;
         if (posOrigin.x == (isOddY ? posTarget.x - 1 : posTarget.x) && posOrigin.y == posTarget.y + 1) // Left Down
             return Directions.LD;
         if (posOrigin.x == (isOddY ? posTarget.x : posTarget.x + 1) && posOrigin.y == posTarget.y + 1) // Left Up
             return Directions.LU;
 
-        throw new System.Exception("Target Tile is not a valid neighbor of the Origin Tile what should't be possible but yeah");
+        throw new System.Exception("Target Tile is not a valid neighbor of the Origin Tile what shouldn't be possible but yeah");
     }
+
     private bool OriginHasConnectionToTarget(GameObject _origin, GameObject _target)
     {
         Directions neededConnectionDirection = GetConnectionDirection(_origin, _target);
@@ -712,9 +363,7 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
         Directions[] relativeDirections = ConvertDefaultConnectionIntoRotationRelative(absolutTypeDirections, rotSteps);
         return relativeDirections;
     }
-    #endregion
 
-    #region Directions&Rotations
     private Directions[] ConvertDefaultConnectionIntoRotationRelative(Directions[] _directions, int _rotSteps)
     {
         Directions[] relativeDirections = new Directions[_directions.Length];
@@ -759,9 +408,7 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
                 return null;
         }
     }
-    #endregion
 
-    #region DataConvertion
     private GameObject PosToObj(Vector2Int _pos)
     {
         return tileField[(int)_pos.x, (int)_pos.y];
@@ -769,24 +416,19 @@ public class ElectricityPuzzleLogic : SolvableObjectBase
 
     private Vector2Int ObjToPos(GameObject _obj)
     {
-        Vector2Int pos = Vector2Int.zero;
-
         for (int x = 0; x < tileField.GetLength(0); x++)
         {
             for (int y = 0; y < tileField.GetLength(1); y++)
             {
                 if (tileField[x, y].Equals(_obj))
                 {
-                    pos.x = x;
-                    pos.y = y;
-                    return pos;
+                    return new Vector2Int(x, y);
                 }
             }
         }
 
-        return pos;
+        return Vector2Int.zero;
     }
-    #endregion
 
     public override bool Solve()
     {
