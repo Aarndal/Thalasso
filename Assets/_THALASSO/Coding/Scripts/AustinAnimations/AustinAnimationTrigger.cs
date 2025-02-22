ccfaf80f32315c50e93faeaa81fd478f3bd742f2
Pascal Keller <Pascal.keller12@gmail.com>
2025-02-22 20:30:41 +0100

    Added dynamic animation speed and added two triggers with animaitons

--- Assets/_THALASSO/Coding/Scripts/AustinAnimations/AustinAnimationTrigger.cs
+++ Assets/_THALASSO/Coding/Scripts/AustinAnimations/AustinAnimationTrigger.cs
@@ -1,4 +1,4 @@
-using System;
+using ProgressionTracking;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -6,7 +6,7 @@ public class AustinAnimationTrigger : MonoBehaviour
 {
     #region References
     private AustinAnimationManager manager;
-    private void OnEnable()
+    private void Awake()
     {
         AustinAnimationManager.SendReference += SaveManagerReference;
     }
@@ -21,19 +21,41 @@ public class AustinAnimationTrigger : MonoBehaviour
     #endregion
 
     private SplineContainer splineContainer;
+    private Collider triggerCollider;
 
-    [SerializeField] private float playbackSpeed;
+    [SerializeField] private float playbackSpeed = 7;
+    [SerializeField] private float startOffset = 0;
+    [SerializeField] private bool isProgressDepended = false;
+    [SerializeField] private SO_SolvableData progressData;
 
     private void Start()
     {
         splineContainer = GetComponentInChildren<SplineContainer>();
-    }
+        triggerCollider = GetComponent<Collider>();
+        triggerCollider.enabled = true;
 
+        if (isProgressDepended)
+        {
+            triggerCollider.enabled = false;
+            progressData.ValueChanged += OnProgressChanged;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && splineContainer != null)
         {
-            manager.AnimationWasTriggered(splineContainer, playbackSpeed);
+            manager.AnimationWasTriggered(splineContainer, playbackSpeed, startOffset);
+            triggerCollider.enabled = false;
+            if (isProgressDepended)
+            {
+                progressData.ValueChanged -= OnProgressChanged;
+            }
         }
     }
+    private void OnProgressChanged(uint _id, bool _isSolved)
+    {
+        triggerCollider.enabled = true;
+    }
+
+
 }
