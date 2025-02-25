25c7b546797c5a401c696b80fa193d74cbca2c09
Aarndal <aarndal@duck.com>
2025-02-25 16:10:20 +0100

    Changed SwitchMusic script to TriggerSoundSwitch. Added TriggerSoundSwitch script to Door prefabs.

---
+++ Assets/_THALASSO/Coding/Scripts/WwiseHelperScripts/TriggerSoundSwitch.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class TriggerSoundSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private AK.Wwise.Switch _onEnterSwitchTo;
+    [SerializeField]
+    private AK.Wwise.Switch _onStaySwitchTo;
+    [SerializeField]
+    private AK.Wwise.Switch _onExitSwitchTo;
+
+    private Collider _collider = default;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    private void Start()
+    {
+        _collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other) => ChangeValueOfAkSoundBank(other, _onEnterSwitchTo);
+
+    private void OnTriggerStay(Collider other) => ChangeValueOfAkSoundBank(other, _onStaySwitchTo);
+
+    private void OnTriggerExit(Collider other) => ChangeValueOfAkSoundBank(other, _onExitSwitchTo);
+
+    private void ChangeValueOfAkSoundBank(Collider collider, AK.Wwise.Switch @switch)
+    {
+        if (@switch != null)
+        {
+            if (collider.TryGetComponent<AkBank>(out _))
+            {
+                @switch.SetValue(collider.gameObject);
+            }
+        }
+    }
+}
