c26c9f0da4022042050fcd614a3ed955c5d1eb86
Aaron Dahlhauser <aarndal@duck.com>
2025-03-04 03:35:48 +0100

    UIInteractionCommand now works with ProgressableInteractiveTrigger as intended.

--- Assets/_THALASSO/Coding/Scripts/InteractionSystem/InteractiveTrigger.cs
+++ Assets/_THALASSO/Coding/Scripts/InteractionSystem/InteractiveTrigger.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class InteractiveTrigger : TriggerBase, IAmInteractive
 {
+    private Collider _interactiveCollider = default;
+
     public bool IsActivatable => IsTriggerable;
 
     protected virtual void Awake()
     {
         if (LayerMask.LayerToName(gameObject.layer) != "InteractiveObject" && LayerMask.NameToLayer("InteractiveObject") == 20)
             gameObject.layer = LayerMask.NameToLayer("InteractiveObject");
+
+        _interactiveCollider = _interactiveCollider != null ? _interactiveCollider : GetComponent < Collider>();
     }
 
     private void Reset()
--- Assets/_THALASSO/Coding/Scripts/InteractionSystem/UIInteractionCommand.cs
+++ Assets/_THALASSO/Coding/Scripts/InteractionSystem/UIInteractionCommand.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,10 +16,17 @@ public class UIInteractionCommand : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _text = default;
 
+    [SerializeField, Tooltip("In Milliseconds")]
+    private int _delayTime = 500;
+    [SerializeField]
+    private Color _defaultColor = Color.white;
     [SerializeField]
     private Color _activatableColor = Color.green;
     [SerializeField]
     private Color _nonActivatableColor = Color.red;
+
+    private IAmInteractive _currentInteractiveObject = default;
+    private bool _isChangingColor = false;
 
     #region UnityLifecycleMethods
     private void Awake()
@@ -33,15 +43,45 @@ public class UIInteractionCommand : MonoBehaviour
             _interactionButton.SetActive(false);
 
         GlobalEventBus.Register(GlobalEvents.Player.InteractiveTargetChanged, OnInteractiveTargetChanged);
+
+        _input.InteractIsTriggered += OnInteractIsTriggered;
         _input.ActionMapChanged += OnActionMapChanged;
+    }
+
+    private void Reset()
+    {
+        _text = _text != null ? _text : GetComponentInChildren<TextMeshProUGUI>();
+
+        _text.color = _defaultColor;
+    }
+
+    private void Start()
+    {
+        _text.color = _defaultColor;
     }
 
     private void OnDisable()
     {
         GlobalEventBus.Deregister(GlobalEvents.Player.InteractiveTargetChanged, OnInteractiveTargetChanged);
+
+        _input.InteractIsTriggered -= OnInteractIsTriggered;
         _input.ActionMapChanged -= OnActionMapChanged;
     }
     #endregion
+
+    private async void ChangeColorForMilliseconds(UnityEngine.UI.Graphic graphic, Color newColor)
+    {
+        if (_isChangingColor)
+            return;
+
+        _isChangingColor = true;
+        graphic.color = newColor;
+
+        await Task.Delay(_delayTime);
+
+        graphic.color = _defaultColor;
+        _isChangingColor = false;
+    }
 
     private void OnActionMapChanged(InputActionMap previousMap, InputActionMap currentMap)
     {
@@ -51,22 +91,25 @@ public class UIInteractionCommand : MonoBehaviour
             _dot.SetActive(true);
     }
 
+    private void OnInteractIsTriggered(bool isTriggered)
+    {
+        if (isTriggered)
+            if (_currentInteractiveObject.IsActivatable)
+                ChangeColorForMilliseconds(_text, _activatableColor);
+            else
+                ChangeColorForMilliseconds(_text, _nonActivatableColor);
+    }
+
     private void OnInteractiveTargetChanged(object[] args)
     {
         foreach (var arg in args)
         {
-            if (arg is Transform)
+            if (arg is IAmInteractive interactiveObject)
             {
                 if (!_interactionButton.activeInHierarchy)
                     _interactionButton.SetActive(true);
-            }
 
-            if (arg is IAmInteractive interactiveObject)
-            {
-                if (interactiveObject.IsActivatable)
-                    _text.color = _activatableColor;
-                else
-                    _text.color = _nonActivatableColor;
+                _currentInteractiveObject = interactiveObject;
             }
 
             if (arg is null)
