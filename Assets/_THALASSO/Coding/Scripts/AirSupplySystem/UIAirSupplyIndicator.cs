580a2f9c129c4278793bd89f75e44aeb9c2ded3c
Aaron Dahlhauser <aarndal@duck.com>
2025-09-23 22:53:21 +0200

    Added UIAirSupplyIndicator class, and created AirSupplyIndicator prefab for InGameUI prefab. Basic function implemented and works.

---
+++ Assets/_THALASSO/Coding/Scripts/AirSupplySystem/UIAirSupplyIndicator.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UIAirSupplyIndicator : MonoBehaviour
+{
+    [SerializeField]
+    private SOAirSupplyData _airSupplyData;
+    [SerializeField]
+    private Image _image;
+
+    private void Awake()
+    {
+        if (_image == null)
+        {
+            if (!TryGetComponent<Image>(out var img))
+            {
+#if UNITY_EDITOR
+                Debug.LogErrorFormat("No Image component found on {0}. Please assign an Image component in the inspector.", gameObject.name);
+#endif
+            }
+            _image = img;
+        }
+
+        if (_airSupplyData != null)
+        {
+            _image.fillAmount = _airSupplyData.CurrentAirSupply / _airSupplyData.MaxAirSupply;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (_airSupplyData != null)
+        {
+            _airSupplyData.AirSupplyChanged += OnAirSupplyChanged;
+        }
+    }
+
+    private void OnAirSupplyChanged(float currentAirSupply)
+    {
+        _image.fillAmount = currentAirSupply / _airSupplyData.MaxAirSupply;
+    }
+}
